6da30d81336cdccf01bbea30f91a72f8592002b0
apoorv jain <apoorv127@gmail.com>
2023-12-13 20:24:53 +0530

    Updated

--- src/test/java/org/shadow/Example.java
+++ src/test/java/org/shadow/Example.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -16,9 +17,22 @@ public class Example {
         driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://letcode.in/shadow");
+
+        //Open Shadow root
         String str = "return document.querySelector(\"#open-shadow\").shadowRoot.querySelector(\"#fname\")";
         WebElement str1 =(WebElement)driver.executeScript(str);
         str1.sendKeys("Apoorv");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
+    @AfterMethod
+    void tearDown(){
+    driver.quit();
+    }
+
 }
