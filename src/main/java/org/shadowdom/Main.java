86111613aa14f44360bf5a4758873cba5be7c4ab
apoorv jain <apoorv127@gmail.com>
2023-12-14 19:22:00 +0530

    Change

--- src/main/java/org/shadowdom/Main.java
+++ src/main/java/org/shadowdom/Main.java
@@ -3,5 +3,10 @@ package org.shadowdom;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a,b,c;
+        a=b=c=10;
+        String age ="car", age1 = "motor";
+        System.out.println(b);
+        System.out.println(age1);
     }
 }
