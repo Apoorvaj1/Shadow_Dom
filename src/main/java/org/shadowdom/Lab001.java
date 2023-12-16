5b410035033d7b28bbcc67050cda8395fd71ceed
apoorv jain <apoorv127@gmail.com>
2023-12-16 10:42:37 +0530

    Added

---
+++ src/main/java/org/shadowdom/Lab001.java
@@ -0,0 +1,13 @@
+package org.shadowdom;
+
+public class Lab001 {
+    public static void main(String... args){
+        int a = 12;
+        int b = 13;
+        String s = "Apoorv";
+        System.out.println(a+b+s);
+        // 25Apoorv
+        System.out.println(s+a+b);
+        //Apoorv1213
+    }
+}
