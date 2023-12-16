da3ad4edb4f20dcce8f56670e7a4a64636c154ba
apoorv jain <apoorv127@gmail.com>
2023-12-16 10:52:25 +0530

    Added

---
+++ src/main/java/LAB/Lab0002.java
@@ -0,0 +1,11 @@
+package LAB;
+
+public class Lab0002 {
+    public static void main(String... args){
+        int a = 10;
+
+        // Compound operator
+        a+=20;
+        System.out.println(a);
+    }
+}
---
+++ src/main/java/LAB/Lab0003.java
@@ -0,0 +1,20 @@
+package LAB;
+
+public class Lab0003 {
+    public static void main(String[] args){
+
+        // relationship operator
+        String name = "Apoorv";
+        if (name == "Apoorv1"){
+            System.out.println("TRUE");
+        }
+        else{
+            System.out.println("FALSE");
+        }
+
+        int a =10;
+        int b = 10;
+        boolean c = (a==b);
+        System.out.println(c);
+    }
+}
