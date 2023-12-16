bf5d1ce2810512a34e57e3c26230b061a3156fdc
apoorv jain <apoorv127@gmail.com>
2023-12-16 11:39:28 +0530

    Added

--- src/main/java/LAB/Lab0002.java
+++ src/main/java/LAB/Lab0002.java
@@ -7,5 +7,20 @@ public class Lab0002 {
         // Compound operator
         a+=20;
         System.out.println(a);
+
+        System.out.println(10 == 10);
+        System.out.println('A' == 65);
+        System.out.println('A');
+
+
+        byte b = 65;
+        System.out.println('A' ==b);
+
+        byte b1 = 45;
+        int d1 = b1;
+        System.out.println(d1);
+
+        int aa = 45;
+        byte bb = (byte)aa;
     }
 }
